1fcdc4a9cc108231c97f52c6920ca262a602413e
evge_nik <evge_nik@tut.by>
2020-09-16 15:42:26 +0300

    #27 Application: add dotenv instead commonConfig

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const dotenv = require('dotenv').config();
 const express = require('express');
 const httpStatus = require('http-status');
 
-const common = require('./config/common.json');
 const errorHandler = require('./express/middleware/error-handler');
 const router = require('./express/routes');
 
 const app = express();
 
-app.use(cors(common.cors));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    optionsSuccessStatus: process.env.CORS_OPTIONS_SUCCESS_STATUS
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -24,8 +27,8 @@ app.get('/', (req, res) => {
     res.sendStatus(httpStatus.OK);
 });
 
-app.listen(common.express.port, () => {
-    console.log(`Express server listening on port ${common.express.port}...`);
+app.listen(process.env.EXPRESS_PORT, () => {
+    console.log(`Express server listening on port ${process.env.EXPRESS_PORT}...`);
 });
 
 module.exports = app;

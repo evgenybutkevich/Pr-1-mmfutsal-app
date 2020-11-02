b2c2ec056df1c39fe4247b74378dfa35c3e00d42
evge_nik <evge_nik@tut.by>
2020-11-02 10:51:31 +0300

    Migrations: change migration order

--- sequelize/migrations/20201101000000-create-playerTeamSeason.js
+++ sequelize/migrations/20201101000000-create-playerTeamSeason.js
@@ -28,6 +28,13 @@ module.exports = {
                 },
                 type: DataTypes.INTEGER,
             },
+            resultId: {
+                references: {
+                    model: "results",
+                    key: "id"
+                },
+                type: DataTypes.INTEGER,
+            },
             createdAt: {
                 allowNull: false,
                 defaultValue: DataTypes.fn('now'),

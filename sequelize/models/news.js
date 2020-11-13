e7f4c288a06a35a30cbf506a7ad4a05f023bb77f
evge_nik <evge_nik@tut.by>
2020-11-13 15:28:42 +0300

    Add news model

---
+++ sequelize/models/news.js
@@ -0,0 +1,54 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class News extends Model {
+        static associate(models) {
+            News.belongsTo(models.section);
+            News.belongsTo(models.user);
+        }
+    };
+
+    News.init({
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        heading: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        sectionId: {
+            allowNull: false,
+            field: 'section_id',
+            type: DataTypes.INTEGER,
+        },
+        content: {
+            allowNull: false,
+            type: DataTypes.STRING(32768)
+        },
+        userId: {
+            allowNull: false,
+            field: 'user_id',
+            type: DataTypes.INTEGER
+        },
+        createdAt: {
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+            field: 'created_at',
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+            field: 'updated_at',
+            type: DataTypes.DATE
+        }
+    }, {
+        sequelize,
+        modelName: 'news',
+    });
+
+    return News;
+};

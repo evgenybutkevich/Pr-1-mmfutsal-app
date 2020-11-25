e9b35dee0ce7bab44ab1009e3bab8e1ca154996d
Evgeny <evge_nik@tut.by>
2020-11-25 18:35:21 +0300

    Sequelize: move image url to const

--- sequelize/seeders/04-demo-player.js
+++ sequelize/seeders/04-demo-player.js
@@ -3,12 +3,13 @@ const faker = require('faker');
 module.exports = {
     up: async function (queryInterface, Sequelize) {
         const recordsList = [];
+        const imageUrl = 'https://i.pravatar.cc/150?img=';
 
         for (let i = 1; i <= 50; i++) {
             recordsList.push({
                 firstName: faker.name.firstName(0),
                 lastName: faker.name.lastName(0),
-                avatar: `https://i.pravatar.cc/150?img=${i}`
+                avatar: imageUrl + i
             });
         }
 
